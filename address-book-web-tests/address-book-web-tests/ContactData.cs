d2eea95b8f543d0bcf45165551057e3475429cfe
Olga P <olya-polunina@yandex.ru>
2019-08-25 20:06:10 +0300

    Задание 4: Сделать всё то же самое, но для контактов

---
+++ address-book-web-tests/address-book-web-tests/ContactData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace address_book_web_tests
+{
+    class ContactData
+    {
+        private string firstname;
+        private string lastname;
+
+        public ContactData(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+
+        public string Firstname
+        {
+            get { return firstname; }
+            set { firstname = value; }
+        }
+
+        public string Lastname
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+    }
+}
